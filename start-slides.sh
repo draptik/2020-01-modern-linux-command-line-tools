9a9bddcf1d0da0e1483489aeb8ac0eeccb1b16c3
Patrick Drechsler <patrick@pdrechsler.de>
2020-01-12 15:06:32 +0100

    kitty: use large default font

--- start-slides.sh
+++ start-slides.sh
@@ -5,9 +5,10 @@ KITTY=`which kitty`
 #KITTY_CONFIG=~/.config/kitty/switch-to-solarized-light.sh
 #KITTY_CONFIG=~/.config/kitty/switch-to-github.sh
 KITTY_CONFIG=~/.config/kitty/switch-to-tomorrow-night-bright.sh
+KITTY_MAIN_CONFIG=~/.config/kitty/kitty.large-font.conf
 
 if [[ -f "$KITTY_CONFIG" ]]; then
-    "${KITTY}" --start-as=fullscreen $START_PRESENTATION
+    "${KITTY}" --config=${KITTY_MAIN_CONFIG} --start-as=fullscreen $START_PRESENTATION
 else
     $START_PRESENTATION
 fi
